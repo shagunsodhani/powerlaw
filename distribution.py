d8923cca7bcb336e9176e87cb45a5ce9114b3b08
shagun Sodhani <sshagunsodhani@gmail.com>
2015-12-21 21:18:26 +0530

    Added powerlaw and stretched exponential distribution

--- distribution.py
+++ distribution.py
@@ -1,4 +1,4 @@
-from math import e, log
+from math import e, log, pow
 from random import random
 
 def random_series(n = 1):
@@ -24,14 +24,13 @@ def exponential_series(Lambda = 1.0, n = 1, xmin = 1.0):
 
     Parameters
     ----------
-    Lambda : Float/Integer, Lambda constant for exponential distribution.
+    Lambda : Float/Integer, Lambda constant for the distribution.
         Default value is 1.0
     n : Integer, number of elements to be generated.
         Default value is 1
         If n < 0, then unbounded number of elements are generated.
-    xmin : Float/Integer, xmin for the exponential distribution.
+    xmin : Float/Integer, xmin for the distribution.
         Default value is 1.0
-        If n < 0, then unbounded number of elements are generated.
     """
     
     def mapping(x):
@@ -40,6 +39,52 @@ def exponential_series(Lambda = 1.0, n = 1, xmin = 1.0):
     for x in random_series(n):
         yield mapping(x)
 
+def stretched_exponential_series(Lambda = 1.0, Beta = 1.0, n = 1, xmin = 1.0):
+    """
+    Generator to generate a stream of numbers taken from stretched exponential distribution.
+
+    Parameters
+    ----------
+    Lambda : Float/Integer, Lambda constant for the distribution.
+        Default value is 1.0
+    Beta : Float/Integer, Beta constant for the distribution.
+        Default value is 1.0
+    n : Integer, number of elements to be generated.
+        Default value is 1
+        If n < 0, then unbounded number of elements are generated.
+    xmin : Float/Integer, xmin for the distribution.
+        Default value is 1.0
+    """
+    
+    def mapping(x):
+        return pow( pow(xmin, Beta)-(1.0/Lambda)*log(1.0-x), 1.0/Beta )
+
+    for x in random_series(n):
+        yield mapping(x)
+
+def powerlaw_series(Alpha = 2.0, n = 1, xmin = 1.0):
+    """
+    Generator to generate a stream of numbers taken from powerlaw distribution.
+
+    Parameters
+    ----------
+    Alpha : Float/Integer, Alpha constant for the distribution.
+        Default value is 2.0
+        Alpha value should be greater than 1.0
+    n : Integer, number of elements to be generated.
+        Default value is 1
+        If n < 0, then unbounded number of elements are generated.
+    xmin : Float/Integer, xmin for the distribution.
+        Default value is 1.0
+    """
+    
+    def mapping(x):
+        return xmin * pow( (1.0-x), -1.0/(Alpha - 1.0) )
+
+    for x in random_series(n):
+        yield mapping(x)
+
+
 if __name__ == "__main__":
-    for i in exponential_series(Lambda = 1, n = 10):
+    for i in stretched_exponential_series(n = 10):
         print i
