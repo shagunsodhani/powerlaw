ffade103f59f56bc8cb67171b403a87c0981468f
shagun Sodhani <sshagunsodhani@gmail.com>
2015-12-22 00:23:04 +0530

    Added method to obtain pdf and cdf given a dostribution

--- distribution.py
+++ distribution.py
@@ -1,5 +1,7 @@
 from math import e, log, pow
 from random import random
+from regression import least_square_regression
+import matplotlib.pyplot as plt
 
 def random_series(n = 1):
     """
@@ -82,9 +84,120 @@ def powerlaw_series(Alpha = 2.0, n = 1, xmin = 1.0):
         return xmin * pow( (1.0-x), -1.0/(Alpha - 1.0) )
 
     for x in random_series(n):
-        yield mapping(x)
+        yield int(mapping(x))
+
+def pdf(series):
+    """
+    Generator to generates pdf(probability distribution function) of any series.
+
+    Parameters
+    ----------
+    series : list of values.
+
+    Returns
+    -------
+    (key, value) pairs are returned where key is one of the entries from the input series and value is the corresponding pdf for the key. 
+        The pairs are sorted by key.
+    """
+
+    sorted_series = sorted(series)
+    key_to_return = -1
+    value_to_return = 0
+    for key in sorted_series:
+        if(key>key_to_return and key_to_return!=-1):
+            yield (key_to_return, value_to_return)
+            key_to_return = key
+            value_to_return = 1
+        else:
+            key_to_return = key
+            value_to_return+=1
+    yield (key_to_return, value_to_return)
+
+def cdf(series):
+    """
+    Generator to generates cdf(cummulative distribution function) of any series.
+
+    Parameters
+    ----------
+    series : list of values.
+
+    Returns
+    -------
+    (key, value) pairs are returned where key is one of the entries from the input series and value is the corresponding pdf for the key. 
+        The pairs are sorted by key.
+    """
+
+    sorted_series = sorted(series)
+    # print sorted_series
+    key_to_return = -1
+    value_to_return = 0
+    for key in sorted_series:
+        if(key>key_to_return and key_to_return!=-1):
+            yield (key_to_return, value_to_return)
+            key_to_return = key
+            value_to_return += 1
+        else:
+            key_to_return = key
+            value_to_return+=1
+    yield (key_to_return, value_to_return)
+
+def plot_pdf_series(series):
+    """
+    Plots pdf(probability distribution function) for any series.
+
+    Parameters
+    ----------
+    series : list of values.
+
+    Returns
+    -------
+    Log log plot of the values.
+    """
+    # sorted_series = sorted(series)
+    x = []
+    y = []
+
+    for (key, value) in pdf(series):
+        x.append(key)
+        y.append(value)
+
+    plt.loglog(x, y,'go', label="original data")
+    plt.show()
+
 
 
 if __name__ == "__main__":
-    for i in stretched_exponential_series(n = 10):
-        print i
+    n = 3
+    # series = powerlaw_series(n = n, xmin = 3, Alpha = 4)
+    series = [i for i in cdf(sorted([int(i) for i in powerlaw_series(n = n, xmin = 2, Alpha = 2.6)]))]
+    #e print y
+    print series
+    # plot_pdf_series(series)
+
+    # plot_pdf_series([i[0] for i in cdf(y)])
+    # if __name__ == "__main__":
+    x = []
+    y = []
+    # result = {}
+    # for i in series:
+    #     if(int(i) not in result):
+    #         result[int(i)] = 1
+    #     else:
+    #         result[int(i)]+=1
+
+    # for (key, value) in series:
+    #     print key
+    #     print value
+    #     x.append(key)
+    #     y.append(value)
+
+    # plt.loglog(x, y,'go', label="original data")
+    # plt.show()
+
+    # print x
+    # print y
+    # least_square_regression(x, y)
+    # for i in cdf(exponential_series(n = n, xmin = 0, Lambda = 1) ):
+    #     print i
+
+        
