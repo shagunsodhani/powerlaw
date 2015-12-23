82f774d4d880e9c448267c6321e3af53a58d8243
shagun Sodhani <sshagunsodhani@gmail.com>
2015-12-23 09:25:29 +0530

    Added method to generate ccdf for a series

--- distribution.py
+++ distribution.py
@@ -1,6 +1,6 @@
 from math import e, log, pow
 from random import random
-from regression import least_square_regression
+# from regression import estimate_scaling_parameter, estimate_parameters
 import matplotlib.pyplot as plt
 
 def random_series(n = 1):
@@ -84,17 +84,21 @@ def powerlaw_series(Alpha = 2.0, n = 1, xmin = 1.0):
         return xmin * pow( (1.0-x), -1.0/(Alpha - 1.0) )
 
     for x in random_series(n):
-        yield int(mapping(x))
+        # yield int(mapping(x))
+        yield mapping(x)
 
-def frequency_distribution(series, pdf = True):
+def frequency_distribution(series, pdf = True, ccdf = True):
     """
-    Generator to generates pdf(probability distribution function) or cdf(cummulative distribution function) of any series.
+    Generator to generates pdf(probability distribution function) or cdf(cummulative distribution function) or ccdf(complementary cummulative distribution function) of any series.
 
     Parameters
     ----------
     series : list of values.
 
     pdf : Boolean. If True, return pdf else cdf
+        Default value is True
+
+    ccdf : Boolean. This is considered only if pdf is set to False. If ccdf = True, return ccdf else cdf
         Default value is True
 
     Returns
@@ -105,20 +109,42 @@ def frequency_distribution(series, pdf = True):
 
     sorted_series = sorted(series)
     key_to_return = -1
-    value_to_return = 0
-    for key in sorted_series:
-        if(key>key_to_return and key_to_return!=-1):
-            yield (key_to_return, value_to_return)
-            key_to_return = key
-            if(pdf):
-                value_to_return = 1
-            else:
-                value_to_return+=1
-        else:
-            key_to_return = key
-            value_to_return+=1
-    yield (key_to_return, value_to_return)
 
+    if(pdf==True or ccdf==False):
+        
+        if(pdf==True):
+            def update_value (value):
+                return 1
+        else:
+            def update_value (value):
+                return value+1
+        
+        value_to_return = 0
+        for key in sorted_series:
+        # print key
+            if(key>key_to_return and key_to_return!=-1):
+                yield (key_to_return, value_to_return)
+                key_to_return = key
+                value_to_return = update_value(value_to_return)
+            else:
+                key_to_return = key
+                value_to_return+=1
+        yield (key_to_return, value_to_return)
+
+    else:
+        count = len(sorted_series)
+        # print sorted_series
+        value_to_deduct = 0
+        for key in sorted_series:
+            if(key>key_to_return and key_to_return!=-1):
+                yield (key_to_return, count)
+                count -=value_to_deduct
+                key_to_return = key
+                value_to_deduct = 1
+            else:
+                key_to_return = key
+                value_to_deduct+=1
+        yield (key_to_return, count)
 
 def plot_f_series(series):
     """
@@ -145,10 +171,12 @@ def plot_f_series(series):
 
 
 if __name__ == "__main__":
-    n = 10
+    n = 100000
     # series = powerlaw_series(n = n, xmin = 3, Alpha = 4)
-    series = [i for i in frequency_distribution(sorted([int(i) for i in powerlaw_series(n = n, xmin = 2, Alpha = 2.6)]), pdf=False)]
+    # series = [i for i in frequency_distribution(sorted([int(i) for i in powerlaw_series(n = n, xmin = 2, Alpha = 2.6)]), pdf=False)]
     #e print y
-    print series
-
-        
+    # print series
+    # for j in frequency_distribution([int(i) for i in powerlaw_series(n=n, xmin = 2, Alpha = 2.6)], pdf=False, ccdf=True):
+        # print j
+    # print estimate_scaling_parameter(powerlaw_series(n=n, xmin = 2, Alpha = 2.6), xmin = 2, discrete=False)
+    # estimate_parameters(powerlaw_series(n=n, xmin = 2, Alpha = 2.6))
